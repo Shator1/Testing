f26b49ae66b331739a1b8dc3b5b39560972caf32
Shator1 <71104541+Shator1@users.noreply.github.com>
2024-09-17 01:15:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 import * as skinview3d from "skinview3d";
- import { io } from 'https://cdn.socket.io/4.0.0/socket.io.esm.min.js';
+    // Import socket.io-client from CDN
+    import { io } from 'https://cdn.socket.io/4.0.0/socket.io.esm.min.js';
+
+    // Connect to your Glitch backend
+    const socket = io('https://ember-phantom-margin.glitch.me');
 
 const container = document.getElementById('skin-viewer');
 const downloadButton = document.getElementById('download');
